0521cf689d30abefff59840b0454f3d0f7f51451
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-21 19:19:35 +0300

    created TagUpdateView and 'tag_form.html' in templates/list_app

--- list_app/urls.py
+++ list_app/urls.py
@@ -5,10 +5,12 @@ from django.conf import settings
 from list_app.views import (
     index,
     TagListView,
+    TagUpdateView,
 )
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>", TagUpdateView.as_view(), name="tag-update"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "list_app"
--- list_app/views.py
+++ list_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from list_app.models import Task, Tag
@@ -22,3 +23,9 @@ class TagListView(generic.ListView):
     model = Tag
     template_name = "list_app/tag_list.html"
     context_object_name = "tags"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ["name"]
+    success_url = reverse_lazy("list_app:tag-list")
