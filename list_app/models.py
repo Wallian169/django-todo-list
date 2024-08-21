e62dcba965a1d0181474f9ff0fef04df9731a6cb
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-21 17:30:47 +0300

    registered models in admin panel, added test instances, modified models 'str' method

--- list_app/models.py
+++ list_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField(
@@ -12,9 +15,13 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(
         null=True,
+        blank=True,
     )
     done = models.BooleanField(default=False)
     tags = models.ManyToManyField(
         to=Tag,
         related_name="tasks",
     )
+
+    def __str__(self) -> str:
+        return self.content
