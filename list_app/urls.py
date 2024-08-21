15f9e1ccd67de15a95a710ade0cdc9e71dc9a6ed
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-21 17:19:06 +0300

    added support for custom css files

--- list_app/urls.py
+++ list_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from list_app.views import (
     index,
 )
 urlpatterns = [
     path("", index, name="index"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 app_name = "list_app"
